f5fabd4133f25b9a42bf9231372bf64b712cc5c9
cilam3 <cilam@proton.me>
2026-07-14 18:01:51 +0200

    new printing methods

--- http-integration/tarallo.c
+++ http-integration/tarallo.c
@@ -1,7 +1,13 @@
 #include "tarallo.h"
+#include <stdint.h>
 #include <unistd.h>
 #include "display.h"
 #include "keyboard.h"
+#include "spd.h"
+#include "jedec_id.h"
+#include "smbios.h"
+
+// the definitions are copied from the `display.c` module, since they weren't global
 
 #define POP_STAT_R       12
 #define POP_STAT_C       18
@@ -18,6 +24,8 @@ static uint16_t popup_status_save_buffer[POP_STAT_W * POP_STAT_H];
 
 unsigned int seconds = 1;
 
+//it's the main function to communicate with the lan port and subsequently with the tarallo
+
 int communication() {
     sleep(seconds);
     while (get_key() == '\0') {};
@@ -27,6 +35,7 @@ int communication() {
     return 0;
 }
 
+// display in a window all the characteristics of the ram sticks before loading them to the tarallo
 
 void info_display() {
     bool big_status_displayed = false;
@@ -43,8 +52,10 @@ void info_display() {
 
             clear_screen_region(POP_STATUS_REGION);
 
+
+
             {
-                prints(POP_STAT_R+1, POP_STAT_C+5, "");
+                prints(POP_STAT_R+1, POP_STAT_C+5, /*print_spdi_tarallo(spdi, 0)*/"");
                 prints(POP_STAT_R+2, POP_STAT_C+5, "");
                 prints(POP_STAT_R+3, POP_STAT_C+5, "");
                 prints(POP_STAT_R+4, POP_STAT_C+5, "");
@@ -60,4 +71,43 @@ void info_display() {
         set_background_colour(palette.background);
     }
         big_status_displayed = true;
+}
+
+// service function to print the ram sticks characteristics on the screen, it prints in order:
+// Slot X | serial number| size | type | freq | form_factor | if_ecc | manufacturer
+
+void print_spdi_tarallo(spd_info spdi, uint8_t row)
+{
+    uint8_t curcol;
+    uint16_t i;
+
+    // Print Slot Index, Module Size, type & Max frequency (Jedec or XMP)
+    curcol = printf(row, 0, " - Slot %i: %kB %s-%i",
+                    spdi.slot_num,
+                    spdi.module_size * 1024,
+                    spdi.type,
+                    spdi.freq,
+                    form_factor(dmi_memory_device->form));
+
+    // Print ECC status
+    if (spdi.hasECC) {
+        curcol = prints(row, ++curcol, "ECC");
+    }
+
+    // Print Manufacturer from JEDEC106
+    for (i = 0; i < JEP106_CNT; i++) {
+        if (spdi.jedec_code == jep106[i].jedec_code) {
+            curcol = printf(row, ++curcol, "- %s", jep106[i].name);
+            break;
+        }
+    }
+}
+
+const char* form_factor(uint8_t form_code) {
+    switch(form_code) {
+        case 0x09 : return "DIMM";
+        case 0x0D : return "SODIMM";
+        case 0x0F : return "FB-DIMM";
+        default : return "ASD"; // TODO: when sending to tarallo remember to check if "ASD", and if so DO NOT SEND, it's not in the database
+    }
 }
